d6689ae719d92ba035c95d8273e738e5cdadbcb5
sanghwap park <temp@gmail.com>
2026-07-15 15:14:05 +0900

    refactor: 실생산량 계산에 epsilon 보정 적용해 부동소수점 ceil 오차 방지
    shortage=21, yield=0.7처럼 나누어떨어지는 값도 부동소수점 연산 오차로
    21/0.7이 30.000000000000003...이 되어 ceil이 실제보다 1 큰 값을
    반환하는 문제를 확인해 epsilon 보정으로 수정.

--- Project1/Project1/Model/ProductionQueueCalculator.h
+++ Project1/Project1/Model/ProductionQueueCalculator.h
@@ -14,7 +14,12 @@ inline int CalculateActualProductionQuantity(int shortage, double yield)
     {
         return 0;
     }
-    return static_cast<int>(std::ceil(shortage / yield));
+
+    // 예: shortage=21, yield=0.7일 때 21/0.7이 부동소수점 연산에서
+    // 30.000000000000003...으로 계산되어 ceil이 30이 아닌 31을 반환하는
+    // 오차를 막기 위해 epsilon만큼 보정한다.
+    constexpr double Epsilon = 1e-9;
+    return static_cast<int>(std::ceil(shortage / yield - Epsilon));
 }
 
 inline double CalculateTotalProductionTime(double avgProductionTime, int actualProductionQuantity)
