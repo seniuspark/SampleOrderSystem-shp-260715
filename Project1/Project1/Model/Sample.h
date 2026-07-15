d8a02b74c4d21f31e2063bba5ae8ba9234ca8dcc
sanghwap park <temp@gmail.com>
2026-07-15 14:49:46 +0900

    feat: Sample 생성자에 수율 범위 검증 추가(Green)
    Yield가 (0.0, 1.0] 범위를 벗어나면 std::invalid_argument를 던지도록 해
    SampleTest.RejectsYieldOutOfRange를 통과시킨다.

--- Project1/Project1/Model/Sample.h
+++ Project1/Project1/Model/Sample.h
@@ -1,13 +1,21 @@
 #pragma once
 
+#include <stdexcept>
 #include <string>
 
 class Sample
 {
 public:
+    static constexpr double MinYieldExclusive = 0.0;
+    static constexpr double MaxYieldInclusive = 1.0;
+
     Sample(std::string sampleId, std::string name, double avgProductionTime, double yield, int stock)
         : SampleId(std::move(sampleId)), Name(std::move(name)), AvgProductionTime(avgProductionTime), Yield(yield), Stock(stock)
     {
+        if (Yield <= MinYieldExclusive || Yield > MaxYieldInclusive)
+        {
+            throw std::invalid_argument("Yield must be within (0.0, 1.0]");
+        }
     }
 
     std::string SampleId;
